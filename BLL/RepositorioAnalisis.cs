c8de1f3955d486ce6f0ed95b07fe1e6b19577bf9
williambh98 <42818093+williambh98@users.noreply.github.com>
2019-09-08 13:30:22 -0400

    Trabajando

--- BLL/RepositorioAnalisis.cs
+++ BLL/RepositorioAnalisis.cs
@@ -1,6 +1,7 @@
 ﻿using Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,48 @@ namespace BLL
 {
     public class RepositorioAnalisis : RepositorioBase<Analisis>
     {
+        public override Analisis Buscar(int id)
+        {
+            Analisis analisis = new Analisis();
+            try
+            {
+                analisis = _contexto.Analisis.Find(id);
+                analisis.detalle.Count();
+                foreach (var item in analisis.detalle)
+                {
+                    string s = item.TipoAnalisis.Descripcion;
+                }
+                
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return analisis;
+        }
+
+        public override bool Modificar(Analisis analisis)
+        {
+            bool paso = false;
+            try
+            {
+                foreach (var item in analisis.detalle)
+                {
+                    var estado = item.AnalisisID > 0 ? EntityState.Modified : EntityState.Added;
+                    _contexto.Entry(analisis).State = estado;
+
+                }
+
+                _contexto.Entry(analisis).State = EntityState.Modified;
+
+                if (_contexto.SaveChanges() > 0)
+                    paso = true;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
     }
 }
